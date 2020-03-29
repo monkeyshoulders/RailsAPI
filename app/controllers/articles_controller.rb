5b51b02cfadd32645042fe5df6a01c187cec8e95
Matt <mattblesoe2@gmail.com>
2020-03-28 20:04:04 -0400

    added routes and art-controller-spec

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,9 +1,17 @@
 class ArticlesController < ApplicationController
   def index
-    render json: 'hello'
+    render json: 'Heller'
   end
 
   def show
-    render json: "article 1"
+    render json: "These are the droids you are looking for"
+  end
+
+  def update
+    render json: "your patched in"
+  end
+
+  def destroy
+    render json: "EXTERMINATE! EXTERMINATE!"
   end
 end
